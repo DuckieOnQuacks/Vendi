fb356f04c147f043231da617333a2a859f7b73d4
Maxwell Synard <synard.maxwell@gmail.com>
2022-12-11 13:39:10 -0800

    Refactored home_page file

--- app_prototype/lib/home_page.dart
+++ app_prototype/lib/home_page.dart
@@ -1,4 +1,3 @@
-import 'dart:io';
 import 'dart:ui' as ui;
 
 import 'package:camera/camera.dart';
@@ -6,8 +5,7 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:image_picker/image_picker.dart';
-import 'package:vendi_app/home_page.dart';
+import 'package:vendi_app/machine_bottom_sheet.dart';
 
 
 const LatLng currentLocation = LatLng(39.5299, -119.8143);
@@ -22,17 +20,6 @@ class Homepage extends StatefulWidget
 class _HomepageState extends State<Homepage> {
   late GoogleMapController _mapController;
   final Map<String, Marker> _markers = {};
-  late List<CameraDescription> cameras;
-  bool? isChecked = false;
-
-  @override
-  void initState() {
-    super.initState();
-    // Get a list of available cameras on the device
-    availableCameras().then((availableCameras) {
-      cameras = availableCameras;
-    });
-  }
 
   @override
   Widget build(BuildContext context)
@@ -64,7 +51,7 @@ class _HomepageState extends State<Homepage> {
         snippet: "Specific location of machine",
         onTap: () {
           showModalBottomSheet(context: context, builder: (context) {
-            return createBottomSheet();
+            return const MachineBottomSheet();
           });
         },
       ),
@@ -74,107 +61,11 @@ class _HomepageState extends State<Homepage> {
     setState(() {});
   }
 
-  // Method that opens the camera
-  void openCamera() async {
-    // Ensure that there is a camera available on the device
-    if (cameras == null || cameras.isEmpty) {
-      return;
-    }
-
-    // Take the first camera in the list (usually the back camera)
-    CameraDescription camera = cameras[0];
-
-    // Open the camera and store the resulting CameraController
-    CameraController controller = CameraController(camera, ResolutionPreset.high);
-    await controller.initialize();
-
-    // Navigate to the CameraScreen and pass the CameraController to it
-    Navigator.push(
-      context,
-      MaterialPageRoute(
-        builder: (context) => CameraScreen(controller),
-      ),
-    );
-  }
-
   Future<Uint8List> getBytesFromAsset(String path, int width) async {
     ByteData data = await rootBundle.load(path);
     ui.Codec codec = await ui.instantiateImageCodec(data.buffer.asUint8List(), targetWidth: width);
     ui.FrameInfo fi = await codec.getNextFrame();
     return (await fi.image.toByteData(format: ui.ImageByteFormat.png))!.buffer.asUint8List();
-  }
-
-  // Bottom sheet widget. It will be the basic container for the template information of a vending machine.
-// This can be edited as you wish.
-  Widget createBottomSheet() {
-    return Center(
-      child: Column(
-        children: [
-          // Bottom sheets cannot be updated from outside the page so a stateful builder is needed to handle state changes.
-          // Currently this checkbox should only work for a single vending machine. Whether or not this should be expanded is tbd.
-          StatefulBuilder(
-            builder: (BuildContext context, setState) {
-              return Checkbox(value: isChecked, onChanged: (bool? checked) {
-                setState(() {
-                  isChecked = checked;
-                });
-              });
-            },
-          ),
-          ElevatedButton(onPressed: () {
-            Navigator.pop(context);
-          }, child: const Text("Close Menu")),
-          IconButton(
-            onPressed: () {
-              openCamera();
-            }, icon: const Icon(Icons.camera_alt),
-          ),
-        ],
-      )
-    );
-  }
-}
-
-
-// This is an example CameraScreen widget that you could use to display
-// the camera preview and take pictures. You would need to import the
-// necessary packages and create this widget in your app.
-class CameraScreen extends StatefulWidget {
-  CameraScreen(this.controller);
-
-  final CameraController controller;
-
-  @override
-  _CameraScreenState createState() => _CameraScreenState();
-}
-
-class _CameraScreenState extends State<CameraScreen> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: CameraPreview(widget.controller),
-      // Add a floating action button to take pictures
-      floatingActionButton: FloatingActionButton(
-        onPressed: () async {
-          try {
-            // Take a picture and store it as a file
-            var image = await widget.controller.takePicture();
-
-            await Navigator.of(context).push(MaterialPageRoute(builder: (BuildContext context) {
-              return Scaffold(
-                appBar: AppBar(title: const Text("Displayed Image")),
-                body: Image.file(File(image.path)),
-              );
-            }
-            ));
-          } catch (e) {
-            // If an error occurs, log the error to the console
-            debugPrint(e.toString());
-          }
-        },
-        child: const Icon(Icons.camera),
-      ),
-    );
   }
 }
 
---
+++ app_prototype/lib/machine_bottom_sheet.dart
@@ -0,0 +1,118 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:camera/camera.dart';
+
+class MachineBottomSheet extends StatefulWidget {
+  const MachineBottomSheet({super.key});
+
+  @override
+  State<MachineBottomSheet> createState() => _MachineBottomSheetState();
+}
+
+class _MachineBottomSheetState extends State<MachineBottomSheet> {
+  late List<CameraDescription> cameras;
+
+  @override
+  void initState() {
+    super.initState();
+    // Get a list of available cameras on the device
+    availableCameras().then((availableCameras) {
+      cameras = availableCameras;
+    });
+  }
+
+  bool? isChecked = false;
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        children: [
+          // Bottom sheets cannot be updated from outside the page so a stateful builder is needed to handle state changes.
+          // Currently this checkbox should only work for a single vending machine. Whether or not this should be expanded is tbd.
+          StatefulBuilder(
+            builder: (BuildContext context, setState) {
+              return Checkbox(value: isChecked, onChanged: (bool? checked) {
+                setState(() {
+                  isChecked = checked;
+                });
+              });
+            },
+          ),
+          ElevatedButton(onPressed: () {
+            Navigator.pop(context);
+          }, child: const Text("Close Menu")),
+          IconButton(
+            onPressed: () {
+              openCamera();
+            }, icon: const Icon(Icons.camera_alt),
+          ),
+          
+        ],
+      )
+    );
+  }
+    // Method that opens the camera
+  void openCamera() async {
+    // Ensure that there is a camera available on the device
+    if (cameras == null || cameras.isEmpty) {
+      return;
+    }
+
+    // Take the first camera in the list (usually the back camera)
+    CameraDescription camera = cameras[0];
+
+    // Open the camera and store the resulting CameraController
+    CameraController controller = CameraController(camera, ResolutionPreset.high);
+    await controller.initialize();
+
+    // Navigate to the CameraScreen and pass the CameraController to it
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => CameraScreen(controller),
+      ),
+    );
+  }
+}
+
+// This is an example CameraScreen widget that you could use to display
+// the camera preview and take pictures. You would need to import the
+// necessary packages and create this widget in your app.
+class CameraScreen extends StatefulWidget {
+  CameraScreen(this.controller);
+
+  final CameraController controller;
+
+  @override
+  _CameraScreenState createState() => _CameraScreenState();
+}
+
+class _CameraScreenState extends State<CameraScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: CameraPreview(widget.controller),
+      // Add a floating action button to take pictures
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          try {
+            // Take a picture and store it as a file
+            var image = await widget.controller.takePicture();
+
+            await Navigator.of(context).push(MaterialPageRoute(builder: (BuildContext context) {
+              return Scaffold(
+                appBar: AppBar(title: const Text("Displayed Image")),
+                body: Image.file(File(image.path)),
+              );
+            }
+            ));
+          } catch (e) {
+            // If an error occurs, log the error to the console
+            debugPrint(e.toString());
+          }
+        },
+        child: const Icon(Icons.camera),
+      ),
+    );
+  }
+}
