35cdb4287c554f3d9b10ab53a2bf7d035fc6653b
DuckieOnQuacks <jojopaschke@gmail.com>
2022-12-10 23:20:07 -0800

    Added profile image

--- app_prototype/lib/profile_page.dart
+++ app_prototype/lib/profile_page.dart
@@ -7,6 +7,28 @@ class ProfilePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("Profile")),
+        body: SingleChildScrollView(
+          child: Container(
+            padding: const EdgeInsets.all(10),
+            child: Column(
+              children: [
+                SizedBox(
+                  width: 120,
+                  height: 120,
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(100),
+                    child: const Image(
+                        image: AssetImage(
+                            'assets/images/2205158_1.jpg'
+                        )
+                    ),
+                  ),
+
+                )
+              ],
+            ),
+          ),
+        )
     );
   }
 }
