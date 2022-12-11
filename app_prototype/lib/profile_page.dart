680ae0ff54c578339adb9434ba43e53c95734a93
DuckieOnQuacks <jojopaschke@gmail.com>
2022-12-11 00:01:14 -0800

    Added to profile page

--- app_prototype/lib/profile_page.dart
+++ app_prototype/lib/profile_page.dart
@@ -1,34 +1,54 @@
 import 'package:flutter/material.dart';
+import 'package:line_awesome_flutter/line_awesome_flutter.dart';
 
 class ProfilePage extends StatelessWidget {
   const ProfilePage({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context)
+  {
     return Scaffold(
       appBar: AppBar(title: const Text("Profile")),
         body: SingleChildScrollView(
-          child: Container(
-            padding: const EdgeInsets.all(10),
-            child: Column(
-              children: [
-                SizedBox(
-                  width: 120,
-                  height: 120,
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(100),
-                    child: const Image(
-                        image: AssetImage(
-                            'assets/images/2205158_1.jpg'
-                        )
+          child: Center(
+            child: Container(
+              padding: const EdgeInsets.all(10),
+              child: Column(
+                children: [
+                  SizedBox(
+                    width: 120,
+                    height: 120,
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(100),
+                      child: const Image(
+                          image: AssetImage(
+                              'assets/images/KermitProfile.jpg'
+                          ),
+                      ),
                     ),
                   ),
-
-                )
-              ],
+                  const SizedBox(height: 10),
+                  Text('Kermit', style: Theme.of(context).textTheme.headline4),
+                  Text('kermit@gmail.com', style: Theme.of(context).textTheme.bodyText2),
+                  const SizedBox(height: 20),
+                  SizedBox(
+                    width: 200,
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.yellow, side: BorderSide.none, shape: const StadiumBorder()),
+                      child: const Text('Edit Profile', style: TextStyle(color: Colors.black)),
+                      ),
+                    ),
+                  const SizedBox(height: 30),
+                  const Divider(),
+                  const SizedBox(height: 10),
+                ],
+              ),
             ),
           ),
         )
+
     );
   }
 }
