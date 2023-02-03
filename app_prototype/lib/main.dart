51938df73ec5649848bfdeedd8a641af2eb970f1
DuckieOnQuacks <jojopaschke@gmail.com>
2023-02-02 18:30:54 -0800

    Updated main

--- app_prototype/lib/main.dart
+++ app_prototype/lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
 import 'login_page.dart';
+import 'package:firebase_core/firebase_core.dart';
+
 
 // All code on this page was developed by the team using the flutter framework
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
 
-void main() {
   runApp(const MyApp());
 }
 
