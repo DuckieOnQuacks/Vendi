f71f9c159f671244c254eeffafa752c5c55e6516
catherineawedin <catherineawedin@gmail.com>
2023-03-06 01:28:51 -0800

    machine bottom sheet redo

--- app_prototype/lib/machine_bottom_sheet.dart
+++ app_prototype/lib/machine_bottom_sheet.dart
@@ -1,8 +1,10 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
-import 'package:camera/camera.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:vendi_app/backend/machine_class.dart';
+import 'package:favorite_button/favorite_button.dart';
+import 'package:flutter/material.dart';
+import 'Addmachine.dart';
 import 'main.dart';
 
 // All code on this page was developed by the team using the flutter framework
@@ -20,168 +22,86 @@ class MachineBottomSheet extends StatefulWidget {
 }
 
 class _MachineBottomSheetState extends State<MachineBottomSheet> {
-  late List<CameraDescription> cameras;
 
-  @override
-  void initState() {
-    super.initState();
-    // Get a list of available cameras on the device
-    availableCameras().then((availableCameras) {
-      cameras = availableCameras;
-    });
-  }
+
+  bool isFavorite = selectedMachine?.isFavorited == 1;
 
   @override
   Widget build(BuildContext context) {
     return Center(
       child: Column(children: [
-        const SizedBox(height: 20),
-        selectedMachine?.imagePath == ''
-            ? Text(
-                'Check the box to add this machine to your favorites. Click the Camera icon to upload a photo!',
-                style: GoogleFonts.bebasNeue(
-                  fontSize: 25,
-                ))
-            : Text('If you\'re happy with the image, click save!',
-                style: GoogleFonts.bebasNeue(fontSize: 25)),
-
         Row(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: [
+          children: <Widget> [
+            Image.asset('assets/images/BlueMachine.png', height: 50),
+            SizedBox(width: 20),
+            Text('Beverage Machine', style: GoogleFonts.bebasNeue(fontSize: 30)),
+            SizedBox(width: 20),
             StatefulBuilder(builder: (BuildContext context, setState) {
               // Checkbox for favoriting the machine
-              return Checkbox(
-                value: selectedMachine?.isFavorited == 1,
-                onChanged: (bool? checked) {
+              return FavoriteButton(
+                isFavorite: isFavorite,
+                valueChanged: (value) {
                   setState(() {
-                    selectedMachine?.isFavorited = checked ?? false ? 1 : 0;
-                    dbHelper.saveMachine(selectedMachine!);
+                    isFavorite = value;
                   });
+                  if (isFavorite) {
+                    selectedMachine?.isFavorited = 1;
+                    dbHelper.saveMachine(selectedMachine!);
+                    // Add to favorites
+                  } else {
+                    // Remove from favorites
+                    selectedMachine?.isFavorited = 0;
+                    dbHelper.saveMachine(selectedMachine!);
+                  }
                 },
               );
             }),
-            // Camera button for opening the camera
-            selectedMachine?.imagePath == ''
-                ? IconButton(
-                    onPressed: () {
-                      openCamera();
-                    },
-                    icon: const Icon(Icons.camera_alt))
-                : ElevatedButton(
-                    onPressed: () {
-                      Navigator.pop(context);
-                    },
-                    child: const Text("Save")),
-          ],
+      ],
         ),
-
         Row(
           mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget> [
+              SizedBox(width: 15),
+              Text('Ansari Building,', style: GoogleFonts.bebasNeue(fontSize: 25)),
+              SizedBox(width: 10),
+              Text('3rd Floor', style: GoogleFonts.bebasNeue(fontSize: 25)),
+    ],
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget> [
+            SizedBox(width: 15),
+            Text('Last updated:', style: GoogleFonts.bebasNeue(fontSize: 25)),
+            SizedBox(width: 10),
+            Text('January 1, 2023', style: GoogleFonts.getFont('Bebas Neue', fontSize: 25, color: Colors.grey[600])),
+            SizedBox(width: 5),
+            Text('12:00pm', style: GoogleFonts.getFont('Bebas Neue', fontSize: 25, color: Colors.grey[600])),
+          ],
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.end,
           children: [
             selectedMachine?.imagePath == ''
                 ? const SizedBox.shrink()
                 : SizedBox(
-                    width: 200,
-                    height: 250,
-                    child: Image.network(selectedMachine!.imagePath),
-                  ),
+              width: 200,
+              height: 250,
+              child: Image.network(selectedMachine!.imagePath),
+            ),
           ],
         ),
 
         // Interactable close menu button
         ElevatedButton(
             onPressed: () {
-              setState(() {
-                //selectedMachine?.imagePath = '';
-              });
-              Navigator.pop(context);
-            },
-            child: const Text("Close Menu"))
-      ]),
-    );
-  }
-
-  // Method that opens the camera
-  void openCamera() async {
-    // Ensure that there is a camera available on the device
-    if (cameras.isEmpty) {
-      return;
-    }
-
-    // Take the first camera in the list (usually the back camera)
-    CameraDescription camera = cameras[0];
-
-    // Open the camera and store the resulting CameraController
-    CameraController controller =
-        CameraController(camera, ResolutionPreset.high);
-    await controller.initialize();
-
-    // Navigate to the CameraScreen and pass the CameraController to it
-    Navigator.push(
-      context,
-      MaterialPageRoute(
-        builder: (context) => CameraScreen(controller),
-      ),
-    ).then((value) => setState(() {}));
-  }
-}
-
-class CameraScreen extends StatefulWidget {
-  CameraScreen(this.controller);
-
-  final CameraController controller;
-
-  @override
-  _CameraScreenState createState() => _CameraScreenState();
-}
-
-class _CameraScreenState extends State<CameraScreen> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: CameraPreview(widget.controller),
-      // Add a floating action button to take pictures
-      floatingActionButton: FloatingActionButton(
-        onPressed: () async {
-          try {
-            // Take a picture and store it as a file
-            var image = await widget.controller.takePicture();
-
-            await Navigator.of(context)
-                .push(MaterialPageRoute(builder: (BuildContext context) {
-              // Checks whether or not the picture is fine for the user
-              return Scaffold(
-                appBar: AppBar(
-                    title: const Text("Is this image ok?"),
-                    automaticallyImplyLeading: false,
-                    leading: IconButton(
-                      icon: const Icon(Icons.close),
-                      onPressed: () {
-                        Navigator.of(context).pop();
-                      },
-                    ),
-                    actions: [
-                      IconButton(
-                          icon: const Icon(Icons.check),
-                          onPressed: () {
-                            setState(() {
-                              Navigator.of(context).pop();
-                              Navigator.of(context).pop();
-                              selectedMachine?.imagePath = image.path;
-                              dbHelper.saveMachine(selectedMachine!);
-                            });
-                          }),
-                    ]),
-                body: Image.file(File(image.path)),
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const AddMachinePage()),
               );
-            }));
-          } catch (e) {
-            // If an error occurs, log the error to the console
-            debugPrint(e.toString());
-          }
-        },
-        child: const Icon(Icons.camera),
-      ),
+            },
+            child: const Text("Update Machine"))
+      ]),
     );
   }
 }
